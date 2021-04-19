c3d9c9a305e99d38b06697a2d1f01cfd616e0750
Beelthazad <eduardocgcadiz@gmail.com>
2021-04-19 20:32:45 +0200

    Fixing pagination on mh-stats

--- mh-stats-api/index.js
+++ mh-stats-api/index.js
@@ -1,4 +1,5 @@
 var BASE_API_PATH_EDU="/api/v1/mh-stats";
+var path = require("path");
 const fs = require('fs');
 var datafile = path.join(__dirname, 'mh-stats-db')
 var DataStore = require('nedb');
