38f329a074baa85e11f031bc78e69b76adbfc649
secaasecas <torresmorenojosecarlos@gmail.com>
2021-04-27 11:41:36 +0200

    seca

---
+++ src/duAPI/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	import DUTable from './duApi/dutable.svelte';
+</script>
+
+
+<main>
+	<h1 class="display-3" style="text-align: center;" >API de uso de drogas</h1>
+	<div>
+		<DUTable/>
+	</div>
+</main>
+
+<style>
+	div {
+		margin-left:50px ;
+		margin-right: 50px;
+	}
+</style>
