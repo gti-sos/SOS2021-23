84b7093b5c5a5b1fdc108df547be4f4d6d83078c
secaasecas <torresmorenojosecarlos@gmail.com>
2021-04-27 15:36:48 +0200

    sec

--- src/App.svelte
+++ src/App.svelte
@@ -3,15 +3,6 @@
 	import Home from "./Home.svelte";
 	import Router from 'svelte-spa-router';
 	import MHST from "./MHST.svelte";
-<<<<<<< HEAD
-	import UnemploymentTable from "./unempApi/unemploymentTable.svelte";
-	import DUTable from "./duApi/DUTable.svelte";
-	const routes = {
-		"/":Home,
-		"/unemployment-stats":UnemploymentTable,
-		"/du-stats": DUTable,
-		"/mh-stats": MHST};
-=======
 	import UnemploymentTable from "./unempAPI/unemploymentTable.svelte";
 	import DUSTATS from "./duAPI/DUTable.svelte";
 
@@ -20,7 +11,6 @@
 		"/unemployment-stats":UnemploymentTable,
 		"/du-stats":DUSTATS,
 		"/mh-stats":MHST};
->>>>>>> c87969489e5962ab854a1684379444ee04c413aa
 </script>
 <main>
 	<Router {routes}> </Router>
