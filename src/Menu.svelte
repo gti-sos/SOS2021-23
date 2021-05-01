c0a4cfe3d12917bc589218160c1d693086c74c2a
Beelthazad <eduardocgcadiz@gmail.com>
2021-05-01 15:49:23 +0200

    Redeploy

--- src/Menu.svelte
+++ src/Menu.svelte
@@ -1,5 +1,5 @@
 <nav class="hidden text-gray-500 uppercase text-bold sm:block">
-    <a style="color:white; font-weight: 600" href="/public/index.html">Sobre nosotros</a>
+    <a style="color:white; font-weight: 600" href="/#/info">Sobre nosotros</a>
       <a style="color:white; font-weight: 600" href="/public/contact.html">Contacto</a>
   </nav>
   <style>
